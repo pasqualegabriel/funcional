2d78e7e9b86e10d53e3fe5992704b961cbdd2a4b
Gabriel Pasquale <pasqualeunq@gmail.com>
2018-08-24 11:22:05 -0300

    continuando practica 2

--- Practica2/Practica2.hs
+++ Practica2/Practica2.hs
@@ -7,7 +7,8 @@
 --Nothing
 --[]
 --let x = [] in x ++ x
---let f x = f x in f [
+--let f x = f x in f []
+--undefined (o bottom )
 
 --Dar ejemplos de expresiones que posean los siguientes tipos:
 --Bool
@@ -40,7 +41,7 @@ alph x = \y -> y
 
 apply :: (a -> b) -> a -> b
 apply f = f
--- apply (+1) 2
+-- apply (1+) 2
 
 twice :: a -> b -> b
 twice x = \y -> y
@@ -49,17 +50,50 @@ flip :: (a -> b -> c) -> b -> a -> c
 flip f = \x -> \y -> f y x
 -- flip (+) 1 2
 
---f) (.) :: (b -> c) -> (a -> b) -> (a -> c)
---g) curry :: ((a,b) -> c) -> a -> b -> c
---h) uncurry :: (a -> b -> c) -> (a,b) -> c
+--(.) :: (b -> c) -> (a -> b) -> (a -> c)
+
+curry :: ((a, b) -> c) -> a -> b -> c
+curry f x y = f (x, y)
+--curry fst 2 3
+
+uncurry :: (a -> b -> c) -> (a, b) -> c
+uncurry f (a, b) = f a b
+--uncurry (+) (1,2)
 
 --4 Recorridos
 --Definir las siguientes funciones de alto orden sobre listas:
---a) map :: (a -> b) -> [a] -> a
---b) filter :: (a -> Bool) -> [a] -> [a]
---c) all :: (a -> Bool) -> [a] -> Bool
---d) any :: (a -> Bool) -> [a] -> Bool
---e) takeWhile :: (a -> Bool) -> [a] -> [a]
+ 
+map2 :: (a -> b) -> [a] -> [b]
+map2 f xs = [ f x | x <- xs]
+
+map3 :: (a -> b) -> [a] -> [b]
+map3 f   []   = []
+map3 f (x:xs) = f x : map3 f xs
+--map2 (1+) [1,2,3,4,5,6]
+
+filter2 :: (a -> Bool) -> [a] -> [a]
+filter2 f xs = [ x | x <- xs, f x ] 
+
+filter3 :: (a -> Bool) -> [a] -> [a]
+filter3 f   []   = []
+filter3 f (x:xs) | f x = x : filter3 f xs
+                 | otherwise = filter3 f xs
+--filter2 (>5) [1,6,2,8,9,3]
+
+all2 :: (a -> Bool) -> [a] -> Bool
+all2 f   []   = True
+all2 f (x:xs) = f x && all2 f xs
+
+any2 :: (a -> Bool) -> [a] -> Bool
+any2 f   []   = False
+any2 f (x:xs) = f x || any2 f xs
+
+takeWhile2 :: (a -> Bool) -> [a] -> [a]
+takeWhile2 f   []   = []
+takeWhile2 f (x:xs) | f x = x : takeWhile2 f xs
+                    | otherwise = takeWhile2 f []
+-- takeWhile2 (>2) [3,4,5,6,2,7,1]
+
 --f) concatMap :: (a -> [b]) -> [a] -> [b]
 --g) partition :: (a -> Bool) -> [a] -> ([a], [a])
 --h) zipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
@@ -81,3 +115,9 @@ flip f = \x -> \y -> f y x
 -- ¿Es posible dar una definición para estas funciones que tenga el beneficio de cortocircuito aún
 --con un orden de evaluación Eager?
 
+-- Extras
+positives :: [Int] -> [Int]
+positives [] = []
+positives (x:xs) | x > 0 = x : positives xs
+                 | otherwise = positives xs
+--positives xs = [ x | x <- xs, x > 0 ]
