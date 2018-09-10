aba0949eb72cba45c9a4261e36263031935f8ef6
Gabriel Pasquale <pasqualeunq@gmail.com>
2018-09-10 17:51:05 -0300

    practica 5 demostraciones simples old

--- Practica5/Practica5.hs
+++ Practica5/Practica5.hs
@@ -1,4 +1,3 @@
-
 --Demostraciones simples
 
 --Demostrar las siguientes equivalencias entre funciones (pueden ser falsas):
@@ -50,31 +49,63 @@ f . f . applyN 0 f = twice f -- def applyN
 (\x -> maybe x id Nothing) = head . (:[])
 
 -- 7 
-curry (uncurry f) = f
+    curry (uncurry f) = f
+curry (uncurry f) x y = f x y -- Principio de extencionalidad  
+     uncurry f (x, y) = f x y -- def curry
+                f x y = f x y -- def uncurry
 
 -- 8
-uncurry (curry f’) = f’
+       uncurry (curry f) = f
+uncurry (curry f) (x, y) = f (x, y) -- Principio de extencionalidad 
+             curry f x y = f (x, y) -- def uncurry
+                f (x, y) = f (x, y) -- def curry
 
 -- 9
 maybe Nothing (Just . const 1) = const (Just 1)
 
 -- 10
-curry snd = curry (fst . swap)
+    curry snd = curry (fst . swap)
+curry snd x y = curry (fst . swap) x y -- Principio de extencionalidad 
+   snd (x, y) = curry (fst . swap) x y -- def curry
+   snd (x, y) = (fst . swap) (x, y)    -- def curry
+            y = (fst . swap) (x, y)    -- def snd
+            y = fst (swap (x, y))      -- def (.)
+            y = fst (y, x)             -- def swap
+            y = y                      -- def fst
 
 -- 11
 (\xs -> null xs || not (null xs)) = const True
 
 -- 12
-flip (curry f) = curry (f . swap)
+    flip (curry f) = curry (f . swap)
+flip (curry f) x y = curry (f . swap) x y -- Principio de extencionalidad 
+       curry f y x = curry (f . swap) x y -- def flip
+          f (y, x) = curry (f . swap) x y -- def curry
+          f (y, x) = (f . swap) (x, y)    -- def curry
+          f (y, x) = f (swap (x, y))      -- def (.)
+          f (y, x) = f (y, x)             -- def swapp
 
 -- 13
-fst = uncurry const
+       fst = uncurry const         
+fst (x, y) = uncurry const (x, y) -- Principio de extencionalidad 
+         y = uncurry const (x, y) -- def fst
+         y = const y x            -- def uncurry
+         y = y                    -- def const
 
 -- 14
-snd = uncurry (flip const)
+       snd = uncurry (flip const)
+snd (x, y) = uncurry (flip const) (x, y) -- Principio de extencionalidad 
+         y = uncurry (flip const) (x, y) -- def snd
+         y = flip const x y              -- def uncurry
+         y = const y x                   -- def flip
+         y = y                           -- def const
 
 -- 15
-swap = uncurry (flip (,))
+       swap = uncurry (flip (,))
+swap (x, y) = uncurry (flip (,)) (x, y) -- Principio de extencionalidad 
+     (y, x) = uncurry (flip (,)) (x, y) -- def swap
+     (y, x) = flip (,) x y              -- def uncurry
+     (y, x) = (y, x)                    -- def flip
 
 -- 16
 or [x] = x || not x
