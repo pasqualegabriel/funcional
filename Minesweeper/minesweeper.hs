ccedfbf9685187cc5391c270b672ca05cd2b0d99
camilacintioli <camila.cintioli@gmail.com>
2018-11-17 14:43:08 -0300

    test

--- Minesweeper/minesweeper.hs
+++ Minesweeper/minesweeper.hs
@@ -46,7 +46,7 @@ replaceC (s:ss) = if s=="Impossible"
 --D= N/C
 --res = N % C
 makeGralBoard:: Int -> Int -> Int -> Int -> Int -> Int -> Board
-makeGralBoard 1 1 0 _ _ _   = ["."]
+makeGralBoard r c 0 n d res = rows r '.' c
 makeGralBoard r c m 1 d res = buildRows r c m 1 d res
 makeGralBoard 1 c m n d res = [(row n '.') ++ (row m '*')] 
 makeGralBoard r 1 m n d res = rows n '.' 1 ++ rows m '*' 1
@@ -56,21 +56,65 @@ makeGralBoard 2 c m n d res = if (n==2||n==3||odd m)
 makeGralBoard r 2 m n d res = if (n==2||n==2||odd m)
 							  then ["Impossible"]
 							  else rows d '.' 2 ++ rows (max 0 (r-d)) '*' 2
-makeGralBoard r c m n d res = if (n>9) ----cambiar 9 por 4.
-							  then haySolucion r c m n d res
-							  else ["Impossible"]
+makeGralBoard r c m n d res = if (n>9) && (n>= 3*c) ----cambiar 9 por 4.
+							  then haySolucion1 r c m n d res
+							  else 	if (n>=9) && (n < 3 * c)
+							  		then haySolucion2 r c m n d res
+							  		else nMenorANueve r c m n d res
 
 
-haySolucion::Int->Int->Int->Int->Int->Int->Board
-haySolucion r c m n d res = if (n>= 3*c)
-							then haySolucion2 r c m n d res
-							else ["a desarrollar"]
+{-
+Ahora nos queda como mucho 9 celdas vacias localizadas en la punta derecha inferior en tablero de 3x3
+En este caso, podemos chequear a MANO que IF n==5||n==7, es IMPOSIBLE.
+Sino, hardcodea una configuracion valida para cada numero de celda vacia en 3x3 cuadrados.
 
-haySolucion2::Int->Int->Int->Int->Int->Int->Board
-haySolucion2 r c m n d res = rows (d-1) '.' c ++ [row (c-1) '.' ++ "*"] ++
+-}
+
+
+
+nMenorANueve:: Int->Int->Int->Int->Int->Int->Board
+nMenorANueve r c m 5 d res = ["Impossible"]
+nMenorANueve r c m 7 d res = ["Impossible"]
+nMenorANueve r c m n d res = haySolucion2 r c m n d res
+
+
+haySolucion1::Int->Int->Int->Int->Int->Int->Board
+haySolucion1 r c m n d res = rows (d-1) '.' c ++ [row (c-1) '.' ++ "*"] ++
 							 [row 2 '.' ++ row (c-2) '*'] ++ rows (r-(d+1)) '*' c --Puede fallar el ultimo
 
---haySolucion3::Int->Int->Int->Int->Int->Int->Board
+
+--R=5 C=6 M=11 N=13 D=2 R=1
+haySolucion2::Int->Int->Int->Int->Int->Int->Board
+haySolucion2 r c m n d res =  if mod m c == 0 
+							  then rows (r - (div m c)) '.' c ++ rows (div m c) '*' c
+							  else if (div m c) + 3 == r
+	 							   then [row (c-1) '.' ++ "*"] ++ [row (c-1) '.' ++ "*"] ++ [row (c-3) '.' ++ "***"] ++ (rows (r-3) '*' c)
+	 							   else rows (r - (div m c) + 3 ) '.' c ++ [row (c-1) '.' ++ "*"] ++ [row (c-1) '.' ++ "*"] ++ rows (div m c) '*'  c ++ (rows (r-3) '*' c)
+
+
+--minasQueFaltan = m-((r-3)*c)
+--minasQuePusiste = (r-3)*c
+
+
+
+
+{-HAYSOLUCION3 XXXXXXXXXXX
+n<3*c && n>=9 => llena r-3 de '*'
+tres filas restantes => llenar las minas q faltan columna x columna de der a izq. 
+SI mnq en c ==2, la ultima mina se pone en la proxima columna.
+
+c....*
+.....*
+...***
+
+
+
+
+******
+
+
+
+-}
 
 
 {-
@@ -83,28 +127,11 @@ si n=> 3*C => llenar de minas izq a der, arriba a abajo. XXXXX
 nmq >= c || nmq < c-2 => coloca nmq izq a deren esa fila. 
 nmq == c-1 => la ultima mina en la fila que sigue.
 
-c.....		  c.....
-......		  ......
-......  ->	  .....*
-.*****		  ..****
-******		  ******
-
-
--}
-
-{-HAYSOLUCION3
-n<3*c && n>=9 => llena r-3 de '*'
-tres filas restantes => llenar las minas q faltan columna x columna de izq a der. 
-SI mnq en c ==2, la ultimna mina se pone en la proxima columna.
 -}
 
 
-{-
-Ahora nos queda como mucho 9 celdas vacias localizadas en la punta derecha inferior en tablero de 3x3
-En este caso, podemos chequear a MANO que IF n==5||n==7, es IMPOSIBLE.
-Sino, hardcodea una configuracion valida para cada numero de celda vacia en 3x3 cuadrados.
 
--}
+
 
 
 
